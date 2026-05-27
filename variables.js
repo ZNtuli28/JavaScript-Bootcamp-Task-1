b5cb93b27f66a4be9cce932d0e797d1f0b48d5f6
NaMhlalukwana <zntuli954@gmail.com>
2026-05-27 02:45:18 +0200

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,32 @@
+
+let name = "Zinhle Ntuli"; 
+let age = 20; 
+let isStudent = true; 
+
+console.log("Exercise 1:");
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+let favoriteColour = "Baby blue";
+console.log("\nExercise 2:");
+console.log("Original favorite colour:", favoriteColour);
+
+favoriteColour = "Pink";
+console.log("New favorite colour:", favoriteColour);
+
+//Asizame iimbalo
+let num1 = 15;
+let num2 = 5;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("\nExercise 3:");
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+
